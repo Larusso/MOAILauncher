9f5e6d0803386a90287be626f8f9d2d5497ef768
Manfred Endres <manfred.endres@tslarusso.de>
2013-07-14 13:14:19 +0200

    init commit

--- Resources/Shell/moai_run.sh
+++ Resources/Shell/moai_run.sh
@@ -1,9 +1,5 @@
-echo "Hello AS" $MOAI_HOME
+echo "EXECUTE MOAI"
 source ~/.bash_profile
-echo "Hello AS 2" $MOAI_HOME
-echo "$@"
-base=$(dirname "$1")
-echo 'base: ' $base
 cd $base
 pwd
 $MOAI_HOME/bin/moai "$@"
